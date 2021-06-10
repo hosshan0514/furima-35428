5547b70b82f7e4bc9d9bafe7db6b23a93e73626c
hosshan0514 <s.h0bsk0hosshan@gmail.com>
2021-06-10 12:38:42 +0900

    新規登録実装

--- db/migrate/20210609063736_devise_create_users.rb
+++ db/migrate/20210609063736_devise_create_users.rb
@@ -10,7 +10,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :last_name,          null: false
       t.string :first_name,         null: false
       t.string :last_name_kana,     null: false
-      t.string :last_name_kana 
+      t.string :first_name_kana,    null: false
+      t.date   :birthday,           null: false
 
 
       ## Recoverable
