d57576da7c88d216a330a312083fa28601df9e95
hosshan0514 <s.h0bsk0hosshan@gmail.com>
2021-06-11 12:28:28 +0900

    Rubocopによる修正結果

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,8 @@ RSpec.describe User, type: :model do
     before do
       @user = FactoryBot.build(:user)
     end
-    
+
     context '登録できる場合' do
-      
       it 'passwordが６文字以上であれば登録できる' do
         @user.password = 'aaa000'
         @user.password_confirmation = 'aaa000'
@@ -23,19 +22,18 @@ RSpec.describe User, type: :model do
     end
 
     context '登録できない場合' do
-
       it 'nicknameが空だと登録できない' do
         @user.nickname = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
-  
+
       it 'emailが空だと登録できない' do
         @user.email = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
-  
+
       it '重複したemailが存在すると登録できない' do
         @user.save
         another_user = FactoryBot.build(:user, email: @user.email)
@@ -61,7 +59,7 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
-  
+
       it 'passwordが半角数字のみの場合、登録できない' do
         @user.password = '000000'
         @user.password_confirmation = '000000'
@@ -82,13 +80,13 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is invalid')
       end
-  
+
       it 'password_confirmationが空だと登録できない' do
         @user.password = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
-  
+
       it 'passwordとpassword_confirmationが一致しないと登録できない' do
         @user.password = 'abc123'
         @user.password_confirmation = '123abc'
