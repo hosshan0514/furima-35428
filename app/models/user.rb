6e19c214e8cf340626df4e837729762e08106079
hosshan0514 <s.h0bsk0hosshan@gmail.com>
2021-06-11 12:27:12 +0900

    ユーザー管理機能：修正１回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,23 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :items
-  has_many :purchases
+  # has_many :items
+  # has_many :purchases
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates_format_of :password, with: PASSWORD_REGEX
+  
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
 
-  validates :nickname, presence: true
+    with_options  format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+      validates :last_name
+      validates :first_name
+    end
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
-    validates :last_name
-    validates :first_name
+    with_options  format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
   end
-
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
-    validates :last_name_kana
-    validates :first_name_kana
-  end
-
-  validates :birthday, presence: true
 end
