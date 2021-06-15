f188579ed65972dfd117b22ec96eda6e4f86570e
hosshan0514 <s.h0bsk0hosshan@gmail.com>
2021-06-15 21:42:47 +0900

    単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name              { 'test' }
+    information       { Faker::Lorem.sentence }
+    category_id       { '2' }
+    state_id          { '2' }
+    postage_id        { '2' }
+    region_id         { '2' }
+    shipping_date_id  { '2' }
+    price             { '777' }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/sample1.png'), filename: 'sample1.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,97 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品出品機能' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+
+    context '保存できる場合' do
+      it '販売価格が300以上の場合、保存できる' do
+        @item.price = '300'
+        @item.valid?
+        expect(@item).to be_valid
+      end
+  
+      it '販売価格が9999999以下の場合、保存できる' do
+        @item.price = '9999999'
+        @item.valid?
+        expect(@item).to be_valid
+      end
+  
+      it '販売価格が半角数字の場合、保存できる' do
+        @item.price = '777'
+        @item.valid?
+        expect(@item).to be_valid
+      end
+    end
+
+    context '保存できない場合' do
+      it '商品画像が無い場合、保存できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+  
+      it '商品名が無い場合、保存できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+  
+      it 'カテゴリー情報がない場合、保存できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+  
+      it '商品状態の情報がない場合、保存できない' do
+        @item.state_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("State can't be blank")
+      end
+  
+      it '配送料の負担の情報がない場合、保存できない' do
+        @item.postage_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postage can't be blank")
+      end
+  
+      it '発送元の地域の情報がない場合、保存できない' do
+        @item.region_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Region can't be blank")
+      end
+  
+      it '発送までの日数の情報がない場合、保存できない' do
+        @item.shipping_date_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping date can't be blank")
+      end
+  
+      it '販売価格がない場合、保存できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+  
+      it '販売価格が300未満の場合、保存できない' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+  
+      it '販売価格が10000000以上の場合、保存できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+  
+      it '販売価格が半角数字でない場合、保存できない' do
+        @item.price = '７７７'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+    end
+  end
 end
