aac06c5ed8f6ded5d93c00c4ac9c4a5a0c79ad0a
hosshan0514 <s.h0bsk0hosshan@gmail.com>
2021-06-22 19:13:35 +0900

    second_commit

---
+++ app/models/purchase_order.rb
@@ -0,0 +1,20 @@
+class PurchaseOrder
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :state_id, :city, :street_address, :suburb, :phone_number
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :state_id
+    validates :city
+    validates :street_address
+    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/} 
+  end
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Order.create(postal_code: postal_code, state_id: state_id, city: city, street_address: street_address, suburb: suburb, phone_number: phone_number, purchase_id: purchase_id)
+  end
+end
+
